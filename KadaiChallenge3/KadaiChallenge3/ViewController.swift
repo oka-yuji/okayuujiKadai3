8b1119e765d0a5cc0c884314dd29c5741e3d7a77
岡優志 <yu.j.0513@gmail.com>
2021-05-21 22:42:05 +0900

    ymlのルールを変更しました

--- KadaiChallenge3/KadaiChallenge3/ViewController.swift
+++ KadaiChallenge3/KadaiChallenge3/ViewController.swift
@@ -8,14 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet private weak var inputValueLeft: UITextField!
-    @IBOutlet private weak var inputValueRight: UITextField!
-    @IBOutlet private weak var inputValueLabelLeft: UILabel!
-    @IBOutlet private weak var inputValueLabelRight: UILabel!
-    @IBOutlet private weak var sumValue: UILabel!
-    
-    override func viewDidLoad() {
+@IBOutlet private weak var inputValueLeft: UITextField!
+@IBOutlet private weak var inputValueRight: UITextField!
+@IBOutlet private weak var inputValueLabelLeft: UILabel!
+@IBOutlet private weak var inputValueLabelRight: UILabel!
+@IBOutlet private weak var sumValue: UILabel!
+override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+}
+struct Calculate {
+    /// 型変換のエラー
+    enum Error: Swift.Error {
+            case typeConversionError
+        }
+
+    /// addition = 加算
+    /// subtraction = 減算
+    enum Opetator {
+        case addition
+        case subtraction
     }
 }

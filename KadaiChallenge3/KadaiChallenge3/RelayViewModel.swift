084f2b8d080e4fa828299c16585def0076bdf4f3
岡優志 <yu.j.0513@gmail.com>
2021-05-22 20:32:43 +0900

    一旦動くものを完成させました　ここから責務を切り出してみます

---
+++ KadaiChallenge3/KadaiChallenge3/RelayViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RelayViewModel.swift
+//  KadaiChallenge3
+//
+//  Created by 岡優志 on 2021/05/21.
+//
+
+import Foundation
+class RelayViewModel {
+//    let vc = ViewController()
+//    let values = vc.relayValues.map({ Int($0.text ?? "") ?? 0 })
+}
--- KadaiChallenge3/KadaiChallenge3/ViewController.swift
+++ KadaiChallenge3/KadaiChallenge3/ViewController.swift
@@ -8,26 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-@IBOutlet private weak var inputValueLeft: UITextField!
-@IBOutlet private weak var inputValueRight: UITextField!
-@IBOutlet private weak var inputValueLabelLeft: UILabel!
-@IBOutlet private weak var inputValueLabelRight: UILabel!
-@IBOutlet private weak var sumValue: UILabel!
-override func viewDidLoad() {
+    @IBOutlet private weak var inputValueLeft: UITextField!
+    @IBOutlet private weak var inputValueRight: UITextField!
+    @IBOutlet private weak var inputValueLabelLeft: UILabel!
+    @IBOutlet private weak var inputValueLabelRight: UILabel!
+    @IBOutlet private weak var sumValue: UILabel!
+    @IBOutlet private weak var isOpeletorLeft: UISwitch!
+    @IBOutlet private weak var isOpeletorRigth: UISwitch!
+    // TextFieldのデータを配列に入れまとめる
+    var relayValues: [UITextField] {[inputValueLeft, inputValueRight]}
+    override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-}
-struct Calculate {
-    /// 型変換のエラー
-    enum Error: Swift.Error {
-            case typeConversionError
+        for item in relayValues {
+            item.keyboardType = .numberPad
         }
-
-    /// addition = 加算
-    /// subtraction = 減算
-    enum Opetator {
-        case addition
-        case subtraction
+    }
+    @IBAction private func calculateButton(_ sender: UIButton) {
+        let values = relayValues.map({ Int($0.text ?? "") ?? 0 })
+        let num1 = isOpeletorLeft.isOn ? values[0] : -values[0]
+        let num2 = isOpeletorRigth.isOn ? values[1] : -values[1]
+        let sum = num1 + num2
+        inputValueLabelLeft.text = String(num1)
+        inputValueLabelRight.text = String(num2)
+        sumValue.text = String(sum)
     }
 }
+//     演算子のenum
+//    enum Operator {
+//        case addision
+//        case sucraction
+//        // 初期化
+//        init(isOn: Bool) {
+//            switch isOn {
+//            case true: self = .addision
+//            case false: self = .sucraction
+//            }
+//        }
+//        func calculate(ope: Operator, num: Int) {
+//
+//        }
+//    }
+//}

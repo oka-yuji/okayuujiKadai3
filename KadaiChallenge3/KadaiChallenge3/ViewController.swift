00d05a3346eacf48a01b62cb4a16607638877389
岡優志 <yu.j.0513@gmail.com>
2021-05-23 02:59:45 +0900

    前回のFBを参考にenumを使用して分けてみました

--- KadaiChallenge3/KadaiChallenge3/ViewController.swift
+++ KadaiChallenge3/KadaiChallenge3/ViewController.swift
@@ -16,7 +16,8 @@ class ViewController: UIViewController {
     @IBOutlet private weak var isOpeletorLeft: UISwitch!
     @IBOutlet private weak var isOpeletorRigth: UISwitch!
     // TextFieldのデータを配列に入れまとめる
-    var relayValues: [UITextField] {[inputValueLeft, inputValueRight]}
+    private var relayValues: [UITextField] {[inputValueLeft, inputValueRight]}
+    private var isOpelators: [UISwitch] {[isOpeletorLeft, isOpeletorRigth]}
     override func viewDidLoad() {
         super.viewDidLoad()
         for item in relayValues {
@@ -24,28 +25,55 @@ class ViewController: UIViewController {
         }
     }
     @IBAction private func calculateButton(_ sender: UIButton) {
-        let values = relayValues.map({ Int($0.text ?? "") ?? 0 })
-        let num1 = isOpeletorLeft.isOn ? values[0] : -values[0]
-        let num2 = isOpeletorRigth.isOn ? values[1] : -values[1]
-        let sum = num1 + num2
-        inputValueLabelLeft.text = String(num1)
-        inputValueLabelRight.text = String(num2)
-        sumValue.text = String(sum)
+        var values = relayValues.map({ Int($0.text ?? "") ?? 0 })
+        // 解答Pt1
+        //        for num in 0..<values.count {
+        //            values[num] = isOpelators[num].isOn ? values[num] : -values[num]
+        //        }
+        //        let sum = values.reduce(0, +)
+        //        inputValueLabelLeft.text = String(values[0])
+        //        inputValueLabelRight.text = String(values[1])
+        //        sumValue.text = String(sum)
+        //
+        // ここから下は解答Pt2
+        for num in 0..<values.count {
+            values[num] = {
+                guard let ope = Operator(isOn: isOpelators[num].isOn) else {
+                    return 0
+                }
+                switch ope.isOperatorValue(ope: ope, num: values[num]) {
+                case .success(let value):
+                    return value
+                default: return 0
+                }
+            }()
+        }
+        inputValueLabelLeft.text = String(values[0])
+        inputValueLabelRight.text = String(values[1])
+        sumValue.text = String(values.reduce(0, +))
+    }
+    //     演算子のenum
+    enum Operator {
+        case addision
+        case subtraction
+        // 初期化
+        init?(isOn: Bool) {
+            switch isOn {
+            case true: self = .addision
+            case false: self = .subtraction
+            }
+        }
+        /// opeでBoolがtrue = +num, false = -num
+        func isOperatorValue(ope: Operator, num: Int)-> Result<Int, Error> {
+            func apply(_ ope: (Int) -> Int) -> Result<Int, Error> {
+                .success(ope(num))
+            }
+            switch ope {
+            case .addision:
+                return apply(+)
+            case .subtraction:
+                return apply(-)
+            }
+        }
     }
 }
-//     演算子のenum
-//    enum Operator {
-//        case addision
-//        case sucraction
-//        // 初期化
-//        init(isOn: Bool) {
-//            switch isOn {
-//            case true: self = .addision
-//            case false: self = .sucraction
-//            }
-//        }
-//        func calculate(ope: Operator, num: Int) {
-//
-//        }
-//    }
-//}

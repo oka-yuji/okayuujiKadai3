e9f4ac94ca223d8bffe96b55710159341220eccd
岡優志 <yu.j.0513@gmail.com>
2021-05-24 22:02:36 +0900

    カプセル化できる箇所をカプセル化しました。また余分なものは削除しました

--- KadaiChallenge3/KadaiChallenge3/ViewController.swift
+++ KadaiChallenge3/KadaiChallenge3/ViewController.swift
@@ -15,20 +15,22 @@ class ViewController: UIViewController {
     @IBOutlet private weak var sumValue: UILabel!
     @IBOutlet private weak var isOpeletorLeft: UISwitch!
     @IBOutlet private weak var isOpeletorRigth: UISwitch!
-    // TextFieldのデータを配列に入れまとめる
+    // TextFieldのデータを配列
     private var relayValues: [UITextField] {[inputValueLeft, inputValueRight]}
-     var isOpelators: [UISwitch] {[isOpeletorLeft, isOpeletorRigth]}
+    // TextFieldのデータを配列
+    private var isOpelators: [UISwitch] {[isOpeletorLeft, isOpeletorRigth]}
+    private let operetor = Operator.self
     override func viewDidLoad() {
         super.viewDidLoad()
         for item in relayValues {
             item.keyboardType = .numberPad
         }
     }
-//     opeにBool型があればOperatorに従って
-     func calculate(_ values: inout [Int], _ operetor: Operator.Type) {
+    @IBAction private func calculateButton(_ sender: UIButton) {
+        var values = relayValues.map({ Int($0.text ?? "") ?? 0 })
         for num in 0..<values.count {
             values[num] = {
-                guard let ope = operetor.Operator(isOn: isOpelators[num].isOn) else {
+                guard let ope = self.operetor.Operator(isOn: isOpelators[num].isOn) else {
                     return 0
                 }
                 switch ope.isOperatorValue(ope: ope, num: values[num]) {
@@ -38,29 +40,8 @@ class ViewController: UIViewController {
                 }
             }()
         }
-    }
-    @IBAction private func calculateButton(_ sender: UIButton) {
-        var values = relayValues.map({ Int($0.text ?? "") ?? 0 })
-        let operetor = Operator.self
-        calculate(&values, operetor)
         inputValueLabelLeft.text = String(values[0])
         inputValueLabelRight.text = String(values[1])
         sumValue.text = String(values.reduce(0, +))
     }
 }
-
-//func calculate(_ values: inout [Int], _ operetor: Operator.Type) {
-//    let viewController = ViewController.self
-//   for num in 0..<values.count {
-//       values[num] = {
-//        guard let ope = operetor.Operator(isOn: viewController.init().isOpelators[num].isOn) else {
-//               return 0
-//           }
-//           switch ope.isOperatorValue(ope: ope, num: values[num]) {
-//           case .success(let value):
-//               return value
-//           default: return 0
-//           }
-//       }()
-//   }
-//}
